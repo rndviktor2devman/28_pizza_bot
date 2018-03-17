009633876fc3f8d931b8dec254c33cd8deaf8cb9
rndviktor2devman <7irb291wqkmu@mail.ru>
2018-03-18 00:08:41 +0300

    now we can load data

--- models.py
+++ models.py
@@ -1,34 +1,44 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import UserMixin, RoleMixin
-
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
-pizza_choice = db.Table(
-    'pizza_choice',
-    db.Column('pizza_id', db.Integer(), db.ForeignKey('pizza.id')),
-    db.Column('choice_id', db.Integer(), db.ForeignKey('choice.id'))
-)
+# pizza_choice = db.Table(
+#     'pizza_choice',
+#     db.Column('pizza_id', db.Integer(), db.ForeignKey('pizza.id')),
+#     db.Column('choice_id', db.Integer(), db.ForeignKey('choice.id'))
+# )
 
 
-class Pizza(db.Model):
+class Pizza(Base):
+    __tablename__ = 'pizza'
     id = db.Column(db.Integer(), primary_key=True)
     title = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
-    choices = db.relationship('Choice', secondary=pizza_choice, backref=db.backref('pizza', lazy='dynamic'))
+    choices = db.relationship("Choice", back_populates="pizza")
 
     def __repr__(self):
         return 'title=%s, description=%s' % (self.title, self.description)
 
 
-class Choice(db.Model):
+class Choice(Base):
+    __tablename__ = 'choice'
     id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(80), unique=True)
+    title = db.Column(db.String(80))
     price = db.Column(db.Numeric)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    pizza = db.relationship("Pizza", back_populates="choices")
 
     def __repr__(self):
         return 'title=%s, price=%s' % (self.title, self.price)
--- setup_default_db.py
+++ setup_default_db.py
@@ -1,212 +1,35 @@
-catalog = [
-    {
-        'title': 'Маргарита',
-        'description': 'соус,сыр Моцарелла',
-        'choices': [
-            {
-                'title': '30 см (450гр)',
-                'price': 360,
-            },
-            {
-                'title': '40 см (750гр)',
-                'price': 460,
-            }
-        ],
-    },
-    {
-        'title': 'Маргарита <Помидоро>',
-        'description': 'соус,сыр Моцарелла,помидоры',
-        'choices': [
-            {
-                'title': '30см (500гр)',
-                'price': 400,
-            },
-            {
-                'title': '40cм (850гр)',
-                'price': 490,
-            }
-        ],
-    },
-    {
-        'title': 'Грибная',
-        'description': 'соус,сыр Моцарелла,шампиньоны',
-        'choices': [
-            {
-                'title': '30см (500гр)',
-                'price': 430,
-            },
-            {
-                'title': '40см (870гр)',
-                'price': 540,
-            }
-        ],
-    },
-    {
-        'title': 'С ветчиной',
-        'description': 'соус,сыр Моцарелла,ветчина',
-        'choices': [
-            {
-                'title': '30см (500гр)',
-                'price': 430,
-            },
-            {
-                'title': '40см (870гр)',
-                'price': 540,
-            }
-        ],
-    },
-    {
-        'title': 'Ветчина с грибами',
-        'description': 'соус,сыр Моцарелла,шампиньоны,ветчина',
-        'choices': [
-            {
-                'title': '30см (580гр)',
-                'price': 460,
-            },
-            {
-                'title': '40см (930гр)',
-                'price': 570,
-            }
-        ],
-    },
-    {
-        'title': 'Салями',
-        'description': 'соус,сыр Моцарелла,салями',
-        'choices': [
-            {
-                'title': '30см (500гр)',
-                'price': 450,
-            },
-            {
-                'title': '40см (870гр)',
-                'price': 560,
-            }
-        ],
-    },
-    {
-        'title': 'Гавайская',
-        'description': 'соус,сыр Моцарелла,ветчина,ананасы',
-        'choices': [
-            {
-                'title': '30см (580гр)',
-                'price': 470,
-            },
-            {
-                'title': '40см (870гр)',
-                'price': 580,
-            }
-        ],
-    },
-    {
-        'title': 'Четыре сыра',
-        'description': 'соус,сыр Моцарелла,сыр Гауда,сыр Эдам,сыр Дор-Блю',
-        'choices': [
-            {
-                'title': '30см (600гр)',
-                'price': 470,
-            },
-            {
-                'title': '40см (870гр)',
-                'price': 580,
-            }
-        ],
-    },
-    {
-        'title': 'Пеперонни',
-        'description': 'соус,сыр Моцарелла,пепперони,перец сладкий',
-        'choices': [
-            {
-                'title': '30см (550гр)',
-                'price': 470,
-            },
-            {
-                'title': '40см (920гр)',
-                'price': 600,
-            }
-        ],
-    },
-    {
-        'title': 'С курицей',
-        'description': 'соус,сыр Моцарелла,куриные грудки,помидоры,шампиньоны перец',
-        'choices': [
-            {
-                'title': '30см (550гр)',
-                'price': 500,
-            },
-            {
-                'title': '40см (920гр)',
-                'price': 610,
-            }
-        ],
-    },
-    {
-        'title': 'Овощная',
-        'description': 'соус,сырМоцарелла,перец,лук,шампиньоны,кукуруза,маслины',
-        'choices': [
-            {
-                'title': '30см (600гр)',
-                'price': 470,
-            },
-            {
-                'title': '40см (920гр)',
-                'price': 580,
-            }
-        ],
-    },
-    {
-        'title': 'Четыре сезона',
-        'description': 'соус,сыр Моцарелла,1/4 салями,1/4 мясная,1/4 ветчина грибы,1/4 четыре',
-        'choices': [
-            {
-                'title': '30см (600гр)',
-                'price': 500,
-            },
-            {
-                'title': '40см (930гр)',
-                'price': 620,
-            }
-        ],
-    },
-    {
-        'title': 'Кальцоне',
-        'description': 'соус, сыр Моцарелла, ветчина, грибы, чесночное масло, кунжут',
-        'choices': [
-            {
-                'title': '30см (520гр)',
-                'price': 440,
-            },
-            {
-                'title': '40см (870гр)',
-                'price': 530,
-            }
-        ],
-    },
-    {
-        'title': 'С тунцом',
-        'description': 'соус, сыр Моцарелла, тунец консервированный, помидоры, лук репчатый, маслины',
-        'choices': [
-            {
-                'title': '30см (570гр)',
-                'price': 470,
-            },
-            {
-                'title': '40см (950гр)',
-                'price': 570,
-            }
-        ],
-    },
-    {
-        'title': 'С беконом',
-        'description': 'соус,сыр,Моцарелла,бекон,помидоры,маслины',
-        'choices': [
-            {
-                'title': '30см (570гр)',
-                'price': 470,
-            },
-            {
-                'title': '40см (950гр)',
-                'price': 590,
-            }
-        ],
-    },
-]
+from models import Base, engine, session, Pizza, Choice
+import json
+import argparse
+import os
+
+
+def get_meals_catalog_path():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('meals_catalog_path', help='Mandatory parametr')
+    return parser.parse_args()
+
+
+def load_meals_catalog_from_file(meals_catalog_path):
+    if not os.path.exists(meals_catalog_path):
+        return None
+    with open(meals_catalog_path, 'r') as meals_catalog:
+        return json.loads(meals_catalog.read())
+
+
+def insert_into_db(items):
+    for item in items:
+        pizza = Pizza(title=item['title'], description=item['description'])
+        session.add(pizza)
+        for choise in item['choices']:
+            option = Choice(title=choise['title'], price=choise['price'])
+            pizza.choices.append(option)
+        session.add(pizza)
+    session.commit()
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    meals_catalog = get_meals_catalog_path()
+    meals = load_meals_catalog_from_file(meals_catalog.meals_catalog_path)
+insert_into_db(meals)
