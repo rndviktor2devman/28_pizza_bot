c3c6a15e61e50fb6e1abe9d9a8d1234f86768d0f
rndviktor2devman <7irb291wqkmu@mail.ru>
2018-03-25 00:54:19 +0300

    remarks

--- models.py
+++ models.py
@@ -47,11 +47,11 @@ roles_users = db.Table(
 
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
+    role_name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
     def __str__(self):
-        return self.name
+        return self.role_name
 
 
 class User(db.Model, UserMixin):
